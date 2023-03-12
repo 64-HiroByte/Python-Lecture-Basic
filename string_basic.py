580d1fec1dacbe95951d4cd7d29a7e3506b30bd4
64-HiroByte <higher.ground.rc@gmail.com>
2023-03-12 17:11:22 +0900

    initial commit

---
+++ string_basic.py
@@ -0,0 +1,23 @@
+# 文字列(Strings)
+
+print("hello world")
+print('hello world')
+print('1')
+
+print("I'm fine")
+print('I"m fine')
+
+print("""
+Hello world!!
+How are you doing?
+""")
+
+print('hello \nworld')
+
+print('hello \tworld')
+# \を2つ重ねるとエスケープ文字 \ を出力できる
+print('back slash n: \\n')
+
+print('i\'m fine')
+
+print('hello' + 'world' + '!!')
