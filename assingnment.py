e1aff261759074ba088846843f7ea98ba476473f
64-HiroByte <higher.ground.rc@gmail.com>
2023-03-13 19:13:14 +0900

    .format とfstringの学習

--- assingnment.py
+++ assingnment.py
@@ -10,3 +10,20 @@ print(hello + world)
 をやると対象の変数名を一度に変更できるので便利
 """
 
+# format
+print('*'*30, ' .format ', '*'*30)
+
+'hello {}'.format('world')
+print('{} {}'.format(hello, world))
+
+name = 'john'
+print('Hey, {}!! How are you doing?'.format(name))
+
+balance = 100
+print('balance: {}'.format(balance))
+
+# fstring <-- 内部的には.formatより処理が早いが、Python 3.6以降で使用可能
+print('*'*30, ' .fstring ', '*'*30)
+print(f'{hello} {world}')
+print(f'Hey, {name}!! How are you doing?')
+print(f'balance: {balance}')
