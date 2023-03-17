d51e6294801f1863c1eb5123adab4721ffc0ceae
64-HiroByte <higher.ground.rc@gmail.com>
2023-03-17 19:19:23 +0900

    数値演算子、エラー、Boolean型、比較演算子の学習

---
+++ error.py
@@ -0,0 +1,9 @@
+# エラーが発生（ZeroDivisionError）
+# result = 100 / 0  # バグ 'bug' <- デバッグ 'debug'
+# print('hello world!!')
+
+# エラーが発生（TypeError）
+# result = '1' + 3  # -> 4? '13'?
+
+# # エラーが発生（NameError）
+print(undefined_variable)
--- numeric.py
+++ numeric.py
@@ -6,3 +6,34 @@ print(type(-100))
 # float(浮動小数点）
 print(type(0.1))
 print(0.1 + 0.1 + 0.1)  # 0.3になるはずだが・・・コンピュータでは完全に0.3にならない
+
+# Numeric Operator（数値演算子）
+# 四則演算
+print(1 + 0.4)
+print(1 - 0.4)
+print(2 * 3)
+print(1 / 2)
+print(5*6 - 3/6)
+
+result = 1 + 1.0
+print(result)
+print(f'type of result: {result} is {type(result)}')
+
+# その他の演算子
+# floor division
+print(14 // 3)
+# modulo, 剰余, あまり
+print(14 % 3)
+# exponentiation べき乗
+print( 2 ** 3)
+
+hit_point = 100
+attack_point = 40.3
+remain = hit_point - attack_point
+print(f'remain hit point is {remain}')
+
+# augmented assignment
+a = 1
+# a = a + 1
+a += 1  # a = a + 1 と同じ
+print(a)
