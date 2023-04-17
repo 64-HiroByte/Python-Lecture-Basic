852e5344d10d72fabe86328b8ad49e061accf710
64-HiroByte <higher.ground.rc@gmail.com>
2023-04-18 06:08:03 +0900

    enumerate()の使い方

---
+++ enumerate.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'peach', 'grapes', 'banana']
+
+for fruit in fruits:
+    print(fruit)
+
+for idx, fruit in enumerate(fruits):
+    print(idx)
+    print(fruit)
+
+for x in enumerate(fruits):
+    print(x)
